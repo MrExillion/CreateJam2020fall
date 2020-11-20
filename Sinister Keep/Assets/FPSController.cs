942bb75746419eb2c54d01e70762852bfca0d495
PEDERGAMERPC\MrExillion <peder@zuricho.dk>
2020-11-20 23:42:19 +0100

    Begin here.

---
+++ Sinister Keep/Assets/FPSController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+
+    public Camera Camera;
+    public Vector2 horizontalVec;
+
+
+
+
+}
