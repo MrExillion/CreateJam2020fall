d9719ae5b7f1e3de557bc227570148c9fc52bbff
PEDERGAMERPC\MrExillion <peder@zuricho.dk>
2020-11-21 04:01:52 +0100

    Core Mechanics minus event system done

--- Sinister Keep/Assets/FPSController.cs
+++ Sinister Keep/Assets/FPSController.cs
@@ -5,10 +5,81 @@ using UnityEngine;
 public class FPSController : MonoBehaviour
 {
 
+    public Vector2 cdGain = new Vector2(100,100);
     public Camera Camera;
     public Vector2 horizontalVec;
+    public Vector2 mouseVec;
+    public float verticalDelta;
+    public float horizontalDelta;
+    public Vector3 moveVec;
+    public Transform playerBody;
+    public Rigidbody rb;
+    public float moveSpeed;
+    public Vector3 jumpforceVec;
+    public float jumpforce;
+    public Transform groundCheck;
+    public float groundDistTolerance;
+    public LayerMask groundMask;
+    public float interactRange;
+
+    private void Start()
+    {
+        Cursor.lockState  = CursorLockMode.Locked;
+    }
 
 
+    void Update()
+    {
 
+        mouseVec.x = Input.GetAxis("Mouse X") * cdGain.x * Time.deltaTime;
+        mouseVec.y = Input.GetAxis("Mouse Y") * cdGain.y * Time.deltaTime;
+
+        horizontalDelta -= mouseVec.x;
+        //horizontalDelta = Mathf.Clamp(horizontalDelta,-90f,90f);
+
+        //transform.localRotation = Quaternion.Euler(horizontalDelta, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseVec.x);
+
+        verticalDelta -= mouseVec.y;
+        verticalDelta = Mathf.Clamp(verticalDelta, -90f, 90f);
+
+        Camera.transform.localRotation = Quaternion.Euler(verticalDelta,0f,0f);
+
+        moveVec.x = Input.GetAxis("Horizontal") * moveSpeed * Time.fixedDeltaTime;
+        moveVec.z = Input.GetAxis("Vertical") * moveSpeed * Time.fixedDeltaTime;
+        //moveVec.y =  rb.velocity.y;//Input.GetAxis("Jump");
+
+        moveVec = moveVec.x * transform.right + moveVec.z * transform.forward;
+        moveVec.y = rb.velocity.y;
+        rb.velocity = moveVec; 
+
+
+        if(Input.GetButtonDown("Jump") && Physics.CheckSphere(groundCheck.position, groundDistTolerance, groundMask))
+        {
+            Debug.Log("JUMP!");
+            jumpforceVec = jumpforce * new Vector3(moveVec.x, 1f, moveVec.z);
+            rb.AddForce(jumpforceVec);
+
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Interact();
+        }
+
+    }
+
+
+    void Interact()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(Camera.transform.position, Camera.transform.forward, out hit, interactRange))
+        {
+            //Debug.Log(hit.transform.GetComponent<PropProperties>().id);
+
+        }
+
+    }
 
 }
---
+++ Sinister Keep/Assets/PropProperties.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropProperties : MonoBehaviour
+{
+
+    public int id;
+    public string name;
+    public bool value;
+    public int counter;
+
+
+
+}
